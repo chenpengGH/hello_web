7df4b509034a7f82864338daeee766cac9d381ec
chenpengGH <307410838@qq.com>
2017-02-08 13:51:38 +0800

    修改  window onscorll 事件

--- js/base.js
+++ js/base.js
@@ -1,22 +1,27 @@
 /**
  * Created by Kong on 2017/2/8.
  */
-window.onload = function () {
-    console.log("onload")
+window.addEventListener("load", function () {
+   console.log("load")
 
     var screenH = document.documentElement.clientHeight || document.body.clientHeight;
 
     var top_a = document.querySelector('#com-d-top-a');
 
-    window.onscroll = function () {
+    window.addEventListener("scroll", function () {
         var scrollTop = document.documentElement.scrollTop || document.body.scrollTop;
         scrollTop >= screenH && (top_a.style.display='block');
         scrollTop < screenH && (top_a.style.display='none');
-    }
+    })
+    // window.onscroll = function () {
+    //     var scrollTop = document.documentElement.scrollTop || document.body.scrollTop;
+    //     scrollTop >= screenH && (top_a.style.display='block');
+    //     scrollTop < screenH && (top_a.style.display='none');
+    // }
 
     top_a.onclick = function () {
         window.scrollTo(0, 0);
         this.blur();
         return false;
     }
-}
+});
